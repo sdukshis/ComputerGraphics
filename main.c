b45490eab44217b326a83360caeca254e9c336b0
Pavel Filonov <filonovpv@gmail.com>
2015-02-17 09:18:20 +0300

    DDA algorithm

---
+++ main.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+
+#include "tga.h"
+
+void swap(int *a, int *b);
+int abs(int a);
+
+/*
+* Using Digital Differential Analyzer algorihm
+* to draw interval connecting (x0, y0) with (x1, y1)
+* on image using color
+*/
+void line (tgaImage *image, 
+           int x0, int y0,
+           int x1, int y1,
+           tgaColor color);
+
+int main(int argc, char **argv)
+{
+    int rv = 0;
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s outfile\n", argv[0]);
+        return -1;
+    }
+    tgaImage * image = tgaNewImage(800, 800, RGB);
+
+    line(image, 100, 50, 20, 600, tgaRGB(255, 255, 255));
+    if (-1 == tgaSaveToFile(image, argv[1])) {
+        perror("tgaSateToFile");
+        rv = -1;
+    }
+
+    tgaFreeImage(image);    
+    return rv;
+}
+
+void line (tgaImage *image, 
+           int x0, int y0,
+           int x1, int y1,
+           tgaColor color)
+{
+    int steep = 0;
+    if (abs(y1 - y0) > abs(x1 - x0)) {
+        steep = 1;
+        swap(&x0, &y0);
+        swap(&x1, &y1);
+    }
+
+    if (x0 > x1) {
+        swap(&x0, &x1);
+        swap(&y0, &y1);
+    }
+
+    int x;
+    double y;
+    double k = ((double)(y1 - y0))/(x1 - x0);
+    for (x = x0, y = y0; x <= x1; ++x, y += k) {
+        if (steep) {
+            tgaSetPixel(image, y, x, color);
+        } else {
+            tgaSetPixel(image, x, y, color);
+        }
+    }
+}
+
+void swap(int *a, int *b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int abs(int a) {
+    return (a >= 0) ? a : -a;
+}
