3d7b050e33bf1746f89756612d9e4c917b65c186
Pavel Filonov <filonovpv@gmail.com>
2015-02-17 10:40:55 +0300

    Render obj file using meshgrid

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "tga.h"
+#include "model.h"
 
 void swap(int *a, int *b);
 int abs(int a);
@@ -15,23 +16,71 @@ void line (tgaImage *image,
            int x1, int y1,
            tgaColor color);
 
+void meshgrid(tgaImage *image, Model *model);
+
 int main(int argc, char **argv)
 {
     int rv = 0;
-    if (argc < 2) {
-        fprintf(stderr, "Usage: %s outfile\n", argv[0]);
+    if (argc < 3) {
+        fprintf(stderr, "Usage: %s <objfile> <outfile>\n", argv[0]);
         return -1;
     }
-    tgaImage * image = tgaNewImage(800, 800, RGB);
 
-    line(image, 100, 50, 20, 600, tgaRGB(255, 255, 255));
-    if (-1 == tgaSaveToFile(image, argv[1])) {
-        perror("tgaSateToFile");
-        rv = -1;
+    Model *model = NULL;
+    tgaImage *image = NULL;
+
+    do {
+        model = loadFromObj(argv[1]);
+        if (!model) {
+            perror("loadFromObj");
+            rv = -1;
+            break;
+        }
+
+        image = tgaNewImage(800, 800, RGB);
+        if (!image) {
+            perror("tgaNewImage");
+            rv = -1;
+            break;
+        }
+
+        meshgrid(image, model);
+
+        if (-1 == tgaSaveToFile(image, argv[2])) {
+            perror("tgaSateToFile");
+            rv = -1;
+            break;
+        }
+    } while (0);
+
+    if (model) {
+        freeModel(model);
     }
-
-    tgaFreeImage(image);    
+    if (image) {
+        tgaFreeImage(image);
+    }  
     return rv;
+}
+
+void meshgrid(tgaImage *image, Model *model)
+{
+    int face, vert, i;
+    int h = image->height;
+    int w = image->width;
+    tgaColor white = tgaRGB(255, 255, 255);
+    Vec3 *p[3];
+    for (face = 0; face < model->nface; ++face) {
+        for (vert = 0; vert < 3; ++vert) {
+            p[vert] = getVertex(model, face, vert);
+        }
+        for (i = 0; i < 3; ++i) {
+            line(image,
+                ((*p[i])[0] + 1.0)*w/2,         ((*p[i])[1] + 1.0)*h/2,
+                ((*p[(i + 1)%3])[0] + 1.0)*w/2, ((*p[(i + 1)%3])[1] + 1.0)*h/2,
+                white);
+        }
+    }
+    tgaFlipVertically(image);
 }
 
 void line (tgaImage *image, 
