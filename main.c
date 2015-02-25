de7b8c6d019a1394dfc173f62e944cd4724816a8
sdukshis <sdukshis@gmail.com>
2015-02-25 21:50:13 +0300

    triangle

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "tga.h"
 #include "model.h"
@@ -16,10 +17,18 @@ void line (tgaImage *image,
            int x1, int y1,
            tgaColor color);
 
+
+void triangle(tgaImage *image,
+              int x0, int y0,
+              int x1, int y1,
+              int x2, int y2,
+              tgaColor color);
 /*
 * Draw model using meshgrid
 */
 void meshgrid(tgaImage *image, Model *model);
+
+void render(tgaImage *image, Model *model);
 
 int main(int argc, char **argv)
 {
@@ -47,7 +56,9 @@ int main(int argc, char **argv)
             break;
         }
 
-        meshgrid(image, model);
+        // triangle(image, 0, 0, 100, 100, 20, 200, tgaRGB(255, 255, 255));
+        // meshgrid(image, model);
+        render(image, model);
 
         if (-1 == tgaSaveToFile(image, argv[2])) {
             perror("tgaSateToFile");
@@ -86,6 +97,27 @@ void meshgrid(tgaImage *image, Model *model)
     tgaFlipVertically(image);
 }
 
+void render(tgaImage *image, Model *model)
+{
+    int face, vert;
+    int h = image->height;
+    int w = image->width;
+    Vec3 *p[3];
+    int screen_coords[3][2];
+    for (face = 0; face < model->nface; ++face) {
+        for (vert = 0; vert < 3; ++vert) {
+            p[vert] = getVertex(model, face, vert);
+            screen_coords[vert][0] = ((*p[vert])[0] + 1.0)*w/2;
+            screen_coords[vert][1] = ((*p[vert])[1] + 1.0)*h/2;
+        }
+        triangle(image, screen_coords[0][0], screen_coords[0][1],
+                        screen_coords[1][0], screen_coords[1][1],
+                        screen_coords[2][0], screen_coords[2][1],
+                        tgaRGB(rand()%256, rand()%256, rand()%256));
+    }
+    tgaFlipVertically(image);    
+}
+
 void line (tgaImage *image, 
            int x0, int y0,
            int x1, int y1,
@@ -112,6 +144,66 @@ void line (tgaImage *image,
         } else {
             tgaSetPixel(image, x, y, color);
         }
+    }
+}
+
+void triangle(tgaImage *image,
+              int x0, int y0,
+              int x1, int y1,
+              int x2, int y2,
+              tgaColor color)
+{
+    /* Sort vertices by y coord */
+    if (y0 > y1) {
+        swap(&x0, &x1);
+        swap(&y0, &y1);
+    }
+    if (y0 > y2) {
+        swap(&x0, &x2);
+        swap(&y0, &y2);
+    }
+    if (y1 > y2) {
+        swap(&x1, &x2);
+        swap(&y1, &y2);
+    }
+
+    /* Compute x coord deltas */
+    double dx01 = 0, dx02 = 0, dx12 = 0;
+    if (y0 != y1) {
+        dx01 = x1 - x0;
+        dx01 /= y1 - y0;
+    }
+    if (y0 != y2) {
+        dx02 = x2 - x0;
+        dx02 /= y2 - y0;
+    }
+    if (y1 != y2) {
+        dx12 = x2 - x1;
+        dx12 /= y2 - y1;
+    }
+    double _dx02 = dx02;
+
+    if (dx01 > dx02) {
+        double t = dx01;
+        dx01 = dx02;
+        dx02 = t;
+    }
+
+    if (dx12 > _dx02) {
+        double t = dx12;
+        dx12 = _dx02;
+        _dx02 = t;
+    }
+
+    /* Fill the triangle */
+    int y, x;
+    double xleft = x0, xright = x0;
+    for (y = y0; y <= y2; ++y) {
+        for (x = xleft; x <= xright; ++x) {
+            tgaSetPixel(image, x, y, color);
+        }
+        xleft += (y <= y1) ? dx01 : _dx02;
+        xright += (y <= y1) ? dx02 : dx12;
     }
 }
 
